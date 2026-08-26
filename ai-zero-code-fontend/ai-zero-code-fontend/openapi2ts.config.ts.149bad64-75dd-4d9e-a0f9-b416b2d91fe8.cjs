8a61a1afb503368cebee4987dec2811b97497ed6
cyx <yxc5586@gmail.com>
2026-08-26 15:05:02 +0800

    配置整理 - 清理本地配置和生成文件

--- ai-zero-code-fontend/ai-zero-code-fontend/openapi2ts.config.ts.149bad64-75dd-4d9e-a0f9-b416b2d91fe8.cjs
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    requestLibPath: "import request from '@/request'",
-    schemaPath: 'http://localhost:8123/api/v3/api-docs',
-    serversPath: './src',
-};
